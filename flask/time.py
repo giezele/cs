6ba765e1ee4cf45d54a9a33d23c7ec04b89c9b66
giezele <giedre.zel@gmail.com>
2021-08-29 12:05:26 +0300

    Lab9

---
+++ flask/time.py
@@ -0,0 +1,14 @@
+# http://flask.pocoo.org/docs/0.12/quickstart
+# http://jinja.pocoo.org/
+
+
+from flask import Flask
+from datetime import datetime
+from pytz import timezone
+
+app = Flask(__name__)
+
+@app.route("/")
+def time():
+    now = datetime.now(timezone('America/New_York'))
+    return "The current date and time in Cambridge is {}".format(now)
